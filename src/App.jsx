4d67ea218a78ab4701b4a029a68dbf99b183bc6d
Madhusudan Kulkarni <96856144+madhusudan-kulkarni@users.noreply.github.com>
2023-07-09 20:52:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen ">
+      <div className="text-4xl text-center">
+        <p>Simple Trip Advisor 🚀</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
